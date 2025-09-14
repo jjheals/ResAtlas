7e38bac34d78d3c3d6499ec602c50fedf0dc33b2
Justin Healey <jjheals@gmail.com>
2025-09-14 10:44:11 -0400

    add helper for setup_logger()

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,5 @@
+import logging 
+import os 
 
 
 def standardize_phone_number(phone_number:str|int) -> str: 
@@ -29,3 +31,35 @@ def standardize_date(date_str:str) -> str:
         return to_datetime(date_str, errors='raise').strftime('%Y-%m-%d %H:%M:%S')
     except Exception:
         raise ValueError(f'Invalid date string: {date_str}')
+    
+
+def setup_logger(log_file_path:str, logger_name:str, min_level:int=logging.DEBUG, log_format:str='%(asctime)s - %(levelname)s: %(message)s') -> logging.Logger:
+    """Sets up a logger to save logs to the given filepath."""
+    
+    # Init a logger and set the lowest level to DEBUG (so all logs are captured)
+    logger:logging.Logger = logging.getLogger(logger_name)
+    logger.setLevel(min_level)
+    
+    # Prevent double logging if root logger is used
+    logger.propagate = False  
+
+    # Avoid duplicate handlers if setup is called multiple times
+    if not logger.handlers:
+        
+        # Create the output dir if it doesn't exist
+        # NOTE: default path if log file path is None or empty string
+        if log_file_path == None or not log_file_path: 
+            log_file_path = './logger_output.log'
+
+        os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+
+        # Create a file handler
+        file_handler:logging.FileHandler = logging.FileHandler(log_file_path, encoding='utf-8')
+        logger.addHandler(file_handler)
+        
+        # Set the format for logs 
+        formatter:logging.Formatter = logging.Formatter(log_format)
+        file_handler.setFormatter(formatter)
+        
+    # Return the logger
+    return logger
